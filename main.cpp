c40943df7e5fb1f941bc5549792890c5679c4b5b
Ahmad <muahmad710@gmail.com>
2022-06-18 11:51:55 +0500

    Adding coin operations

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include"rsa.h"
+#include"ash.h"
+#include<chrono>
+using namespace std;
+
+const int N = 1e5;
+vector<pair<int,int>> networkDelay[N];
+vector<pair<int,int>> transaction[N];
+vector<pair<string, int>> message[N];
+
+int main(int argc, char const *argv[])
+{
+
+    
+
+
+    return 0;
+}
