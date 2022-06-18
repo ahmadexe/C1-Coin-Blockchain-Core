f39888452c2730e078383e5f0ddc36c7bc0c78ac
Ahmad <muahmad710@gmail.com>
2022-06-18 10:42:24 +0500

    Adding ash

--- ash.cpp
+++ ash.cpp
@@ -3,14 +3,25 @@
 
 #include<iostream>
 #include<bits/stdc++.h>
+#include"ash.h"
+
 using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
 
-    
+    string str, str2;
+    cout<<"Enter the message: ";
+    getline(cin, str);
+    cout<<"\n";
+    cout<<"Enter the message: ";
+    getline(cin, str2);
+    cout<<"\n";
 
+    string x = hashStr(str);
+    string y = hashStr(str2);
+    cout<<(x == y);
 
     return 0;
 }
--- ash.h
+++ ash.h
@@ -1,13 +1,34 @@
+// This dumb program is a simple algorithm to hash a string.
+// This hasn't been tested on enough inputs and will surely generate collisions but works for now.
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
+const int a = 31;
+const int b = 15;
 
-int main(int argc, char const *argv[])
+string hashStr(string str)
 {
-
-    
-
-
-    return 0;
+    string hash = "";
+    int i = 0;
+    int j = 1;
+    while(j < str.length())
+    {
+        int x = int(str[i]);
+        int y = int(str[j]);
+        x = x % a;
+        y = y % b;
+        int z = x << y;
+        hash += to_string(z);
+        i++;
+        j++;
+    }
+    string hashS = "";
+    for(int i = 0; i < hash.length(); i++)
+    {
+        int n = hash[i] - 48;
+        hashS += char(n);
+    }
+    return hashS;
 }
