9ba137fafc3b186c765b2ee499f32b26f509b25d
Ahmad <muahmad710@gmail.com>
2022-06-19 12:13:56 +0500

    New Blocks can be added

--- main.cpp
+++ main.cpp
@@ -48,21 +48,79 @@ void addGenesisBlock()
 
 int verifyNonce(int nonceLastBlock)
 {
-    srand(time(0));
-    int random = rand() % nonceLastBlock+2;
+    srand((unsigned) time(0));
+    int random = (rand() % (nonceLastBlock+2));
     return random;
 }
 
-void creatBlock() 
+void creatBlock(string data) 
 {
+    Block* newBlock = new Block;
+    newBlock->index = tail->index + 1;
+    newBlock->previousHash = tail->hash;
+    newBlock->data = data;
+    newBlock->timestamp = to_string(chrono::system_clock::now().time_since_epoch().count());
+    newBlock->nonce = tail->nonce + 1;
+    auto start = chrono::steady_clock::now();
 
+    int checkNonce = verifyNonce(tail->nonce);
+    do{
+        checkNonce = verifyNonce(tail->nonce);
+    } while (checkNonce != newBlock->nonce);    
+    auto end = chrono::steady_clock::now();
+    double elapsedTime = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    elapsedTime = elapsedTime / 1e9;
+    
+    networkDelay[tail->index].push_back(make_pair(elapsedTime, newBlock->index));
+    
+    newBlock->next = NULL;
+    newBlock->previous = tail;
+    newBlock->hash = hashStr(toString(newBlock));
+    tail->next = newBlock;
+    tail->nextHash = newBlock->hash;
+    tail = newBlock;
 }
+
+void printBlocks()
+{
+    Block* temp = genesisBlock;
+    while(temp != NULL)
+    {
+        cout<<"Block "<<temp->index<<"\n";
+        temp = temp->next;
+    }
+}
+
+
 
 int main(int argc, char const *argv[])
 {
 
+    addGenesisBlock();
+    while (true)
+    {   int choice;
+        cout<<"1. Add block: "<<endl;
+        cout<<"2. Print blocks"<<endl;
+        cout<<"3. Exit"<<endl;
+        cin>>choice;
+        string buffer;
+        getline(cin, buffer);
+        if (choice == 1)
+        {
+            string data;
+            cout<<"Enter the data: ";
+            getline(cin, data);
+            creatBlock(data);
+        }
+        else if (choice == 2)
+        {
+            printBlocks();
+        }
+        else if (choice == 3)
+        {
+            break;
+        }        
+    }
     
-
-
     return 0;
 }
